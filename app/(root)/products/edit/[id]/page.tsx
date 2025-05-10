f6cec332fadcb345a93d459b6e8bbe9f90c6a58f
Ayomiidey <quadriayomiidey@gmail.com>
2025-05-10 21:00:51 +0100

    editform fix

--- app/(root)/products/edit/[id]/page.tsx
+++ app/(root)/products/edit/[id]/page.tsx
@@ -1,35 +1,3 @@
-// import {
-//   getProductbyId,
-//   getCategories,
-//   getSuppliers,
-// } from "@/app/lib/actions/product-action";
-// import EditProductForm from "./editForm";
-// import { notFound } from "next/navigation";
-
-// export default async function EditProductPage({
-//   params,
-// }: {
-//   params: { id: string };
-// }) {
-//   const [product, categories, suppliers] = await Promise.all([
-//     getProductbyId(params.id),
-//     getCategories(),
-//     getSuppliers(),
-//   ]);
-
-//   if (!product) {
-//     return notFound();
-//   }
-
-//   return (
-//     <EditProductForm
-//       product={product}
-//       categories={categories}
-//       suppliers={suppliers}
-//     />
-//   );
-// }
-
 import {
   getProductbyId,
   getCategories,
@@ -38,21 +6,20 @@ import {
 import EditProductForm from "./editForm";
 import { notFound } from "next/navigation";
 
-// Correct type for Next.js App Router dynamic route parameters
-interface EditProductPageProps {
-  params: { id: string };
-}
-
 export default async function EditProductPage({
   params,
-}: EditProductPageProps) {
+}: {
+  params: { id: string };
+}) {
   const [product, categories, suppliers] = await Promise.all([
     getProductbyId(params.id),
     getCategories(),
     getSuppliers(),
   ]);
 
-  if (!product) return notFound();
+  if (!product) {
+    return notFound();
+  }
 
   return (
     <EditProductForm
